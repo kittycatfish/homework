f31e7e19ed48a14921bc6c44651d0196fbd0eaf4
kittycatfish <41669700+kittycatfish@users.noreply.github.com>
2021-06-25 02:04:43 +0900

    Update Practice_backend.py

--- Practice_backend.py
+++ Practice_backend.py
@@ -13,36 +13,46 @@ URL = 'https://www.infostock.co.kr/bord.asp'
 dict = {"init" : "init"}
 skwords = {"특징 종목(코스닥)", "특징 종목(코스피)", "특징 상한가"}
 
-driver = webdriver.Edge(executable_path=r"C:\Users\kitty\Documents\Template\prac2\msedgedriver")
-driver.get(url=URL)
-control = driver.find_element_by_xpath("""//*[@id="StartDate"]""")
-control.send_keys("2021-06-15")
-control = driver.find_element_by_xpath("""//*[@id="EndDate"]""")
-control.send_keys("2021-06-15")
-control = driver.find_element_by_xpath("""//*[@id="market_2"]/article/table/tbody[1]/tr/td/table/tbody/tr/td/form/div[2]/input""")
-#control.send_keys("특징 종목(코스닥)")
-#control.send_keys("특징 종목(코스피)")
-control.send_keys("특징 상한가")
-control = driver.find_element_by_xpath("""//*[@id="market_2"]/article/table/tbody[1]/tr/td/table/tbody/tr/td/form/div[2]/button""")
-control.click()
+date = "2021-06-15"
 
-control = driver.find_elements_by_class_name("tr_")[1]
-a_control = control.find_elements_by_tag_name("a")
+driver = webdriver.Edge(executable_path=r"C:\Users\kitty\Documents\Template\prac2\homework\msedgedriver")
 
-for con in a_control :
-    link = con.get_attribute('href')
-    if(link != None) : 
-        con.click()
-        break
+for kw in skwords:
+    driver.get(url=URL)
+    control = driver.find_element_by_xpath("""//*[@id="StartDate"]""")
+    control.send_keys(date)
+    control = driver.find_element_by_xpath("""//*[@id="EndDate"]""")
+    control.send_keys(date)
+    control = driver.find_element_by_xpath("""//*[@id="market_2"]/article/table/tbody[1]/tr/td/table/tbody/tr/td/form/div[2]/input""")
+    control.send_keys(kw)
+    control = driver.find_element_by_xpath("""//*[@id="market_2"]/article/table/tbody[1]/tr/td/table/tbody/tr/td/form/div[2]/button""")
+    control.click()
 
-control = driver.find_element_by_class_name("tbl")
-tr_controls = control.find_elements_by_tag_name("tr")
+    control = driver.find_elements_by_class_name("tr_")[1]
+    a_control = control.find_elements_by_tag_name("a")
 
-for tr_control in tr_controls:
-    str = tr_control.text
-    if("%" in str):
-        txtarr = str.split('\n')
-        dict[txtarr[0]] = txtarr[3]
+    for con in a_control :
+        link = con.get_attribute('href')
+        if(link != None) : 
+            con.click()
+            break
 
-f = open("새파일.txt:", 'w')
+    control = driver.find_element_by_class_name("tbl")
+    tr_controls = control.find_elements_by_tag_name("tr")
+
+    for tr_control in tr_controls:
+        str = tr_control.text
+        if("%)" in str):
+            txtarr = str.split('\n')
+            if(len(txtarr) < 4): continue
+            if(txtarr[0] not in dict):
+                dict[txtarr[0]] = txtarr[3]
+
+filename = date + ".txt"
+f = open(filename, 'w')
+
+for data in dict :
+    str = data + '\t' + dict[data] + '\n'
+    f.write(str)
+
 f.close()
